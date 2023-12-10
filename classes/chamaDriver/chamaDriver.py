69b7eb69bee89855f4838ad136f3b4fa42fcadc6
CaioGunz <caiobarbosa2404@hotmail.com>
2023-12-10 16:25:45 -0300

    Criacao do modulo chamaDriver, foi removido o iniciaDriver da classe pesquisaAnunciosML e movido para a classe chamaDriver onde vai poder ser reapoveitado por outras classes como a classe pesquisaAnuncianteML, essa classe deve ser chamada por meio de importacao

--- classes/chamaDriver/chamaDriver.py
+++ classes/chamaDriver/chamaDriver.py
@@ -1,10 +1,10 @@
-import pandas as pd
-from bs4 import BeautifulSoup
+from selenium import webdriver
+from webdriver_manager.firefox import GeckoDriverManager
+from selenium.webdriver.firefox.service import Service
+from selenium.webdriver.firefox.options import Options
+from selenium.webdriver.common.by import By
 from time import sleep
-from classes.chamaDriver.chamaDriver import iniciaDriver
 
-lista_valores = []
-'''
 class iniciaDriver:
     
     def __init__(self, driver, link):
@@ -33,49 +33,3 @@ class iniciaDriver:
             driver.get(self.link)
         
         return driver
-'''
-class pesquisaMercadoLivre(iniciaDriver):
-    
-    def __init__(self, link, categoria):
-        super().__init__(driver=None, link=link)
-        self.categoria = categoria
-        
-    def novoModeloColeta(self):
-        
-        self.driver = self.chamaDriver()
-        self.driver.get(self.link)
-        
-        while True:
-            page_content = self.driver.page_source
-            
-            site = BeautifulSoup(page_content, 'html.parser')
-            
-            produtos = site.find_all('li', attrs={'class': 'ui-search-layout__item'})
-            
-            for produto in produtos:
-                link = produto.find('a', attrs={'class': 'ui-search-item__group__element ui-search-link__title-card ui-search-link'})
-                categoria = self.categoria
-
-                lista_valores.append([link['href'], categoria])
-                 
-            proximaPagina = site.find('a', attrs={'title': 'Seguinte'})
-            if proximaPagina:
-                proximaPaginaLink = proximaPagina['href']
-                self.driver.get(proximaPaginaLink)
-                sleep(1)
-            else:
-                print('Proxima Pagina Nao Encontrada!!! ENCERRANDO')
-                self.driver.close()
-                break
-        self.salvarDados()
-            
-
-            
-
-    def salvarDados(self):
-        if self.driver is not None:
-            column = ['link', 'categoria']
-            planilhaGerada = pd.DataFrame(lista_valores, columns=column)
-            planilhaGerada.to_csv('planilha.csv', index=False, sep=';')
-            
-            
---
+++ classes/pesquisaAnuncianteMl/pesquisaAnuncianteML.py
@@ -0,0 +1,3 @@
+import pandas as pd
+from time import sleep
+from classes.pesquisaAnunciosMl.pesquisaAnunciosML import iniciaDriver
