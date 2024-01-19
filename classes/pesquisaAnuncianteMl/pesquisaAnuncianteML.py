64a9cad567adb8cae5ca1fecd6750de471dc8e99
Caio Gunz <caiobarbosa2404@hotmail.com>
2024-01-19 15:15:19 -0300

    Correção no link do anuncio para trazer o link correto e não todos os links, correção em CATEGORIA para trazer a categoria que não estava trazendo

--- classes/pesquisaAnuncianteMl/pesquisaAnuncianteML.py
+++ classes/pesquisaAnuncianteMl/pesquisaAnuncianteML.py
@@ -19,21 +19,46 @@ class pesquisaAnuncianteMl(iniciaDriver):
         
         
     def coletaDadosAnunciante(self):
-            
-        titulo = self.driver.find_element(By.XPATH, '//h1[@class="ui-pdp-title"]').text
-        preco = self.driver.find_element(By.XPATH, '//div[@class="ui-pdp-price__second-line"]//span[@class="andes-money-amount__fraction"]').text
-        qtdVendida = self.driver.find_element(By.XPATH, '//span[@class="ui-pdp-subtitle"]').text
-        linkVendedor = self.driver.find_element(By.XPATH, '//a[@class="ui-pdp-media__action ui-box-component__action"]').get_attribute('href')
-        classificacaoVendedor = self.driver.find_element(By.XPATH, '//p[@class="ui-seller-info__status-info__title ui-pdp-seller__status-title"]').text
-        marca = self.driver.find_element(By.XPATH, '//th[contains(., "Marca")]/following-sibling::td//span[@class="andes-table__column--value"]').text
-        sku = self.driver.find_element(By.XPATH, '//th[contains(., "Modelo")]/following-sibling::td//span[@class="andes-table__column--value"]').text
-        categoria = self.categoria
-        link = lista_link
+        
+        sleep(1)
+        try:
+            titulo = self.driver.find_element(By.XPATH, '//h1[@class="ui-pdp-title"]').text
+        except:
+            titulo = 'null'
+        try:
+            preco = self.driver.find_element(By.XPATH, '//div[@class="ui-pdp-price__second-line"]//span[@class="andes-money-amount__fraction"]').text
+        except:
+            preco = 'null'
+        try:
+            qtdVendida = self.driver.find_element(By.XPATH, '//span[@class="ui-pdp-subtitle"]').text
+        except:
+            qtdVendida = 'null'
+        try:
+            linkVendedor = self.driver.find_element(By.XPATH, '//a[@class="ui-pdp-media__action ui-box-component__action"]').get_attribute('href')
+        except:
+            linkVendedor = 'null'
+        try:
+            classificacaoVendedor = self.driver.find_element(By.XPATH, '//p[@class="ui-seller-info__status-info__title ui-pdp-seller__status-title"]').text
+        except:
+            classificacaoVendedor = 'null'
+        try:
+            marca = self.driver.find_element(By.XPATH, '//th[contains(., "Marca")]/following-sibling::td//span[@class="andes-table__column--value"]').text
+        except:
+            marca = 'null'
+        try:
+            sku = self.driver.find_element(By.XPATH, '//th[contains(., "Modelo")]/following-sibling::td//span[@class="andes-table__column--value"]').text
+        except:
+            sku = 'null'
+        try:
+            categoria = self.categoria
+        except:
+            categoria = 'null'
+        try:
+            link = lista_link[-1]
+        except IndexError:
+            link = 'null'    
             
         lista_valores.append([titulo, preco, qtdVendida, linkVendedor, classificacaoVendedor, marca, sku, categoria, link])
-            
-            
-        #return self.pegaLink()
 
     
     
@@ -41,7 +66,7 @@ class pesquisaAnuncianteMl(iniciaDriver):
         
         for index, row in self.links.iterrows():
             link = row['link']
-            categoria = row['categoria']
+            self.categoria = row['categoria']
             lista_link.append(link)
         
             try:
