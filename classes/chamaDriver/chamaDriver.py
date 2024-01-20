9270afc06aa2a25c57a011736399c1cbf429b01e
CaioGunz <caiobarbosa2404@hotmail.com>
2024-01-20 00:22:24 -0300

    Documentacao da classe chamaDriver

--- classes/chamaDriver/chamaDriver.py
+++ classes/chamaDriver/chamaDriver.py
@@ -1,3 +1,4 @@
+#Import das Libs usadas na classe
 from selenium import webdriver
 from webdriver_manager.chrome import ChromeDriverManager
 from selenium.webdriver.chrome.service import Service
@@ -7,11 +8,16 @@ from time import sleep
 
 class iniciaDriver:
     
+    #__init__ cria os atributos driver e link que vai ser usados pelas outras classes
     def __init__(self, driver, link):
         self.driver = driver
         self.link = link
     
+    #Funcao para criar o Browser quando for chamado pelas classes
     def chamaDriver(self):
+        
+        #User-agent utilizado para contornar o erro de HTTP 403, quanto mais user_agent tiver 
+        # menor e a chance do erro acontecer
         user_agent = ("Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/118.0.0.0 Safari/537.36",
                         "Mozilla/5.0 (compatible; msie 7.0; windows nt 5.0; trident/3.1)",
                         "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_6_8) AppleWebKit/535.2 (KHTML, like Gecko) Chrome/15.0.874.54 Safari/535.2",
@@ -24,13 +30,23 @@ class iniciaDriver:
                         "Mozilla/5.0 (windows nt 6.0; win64; x64; rv:7.2) gecko/20100101 firefox/7.2.1",
                         "Mozilla/5.0 (windows; u; windows nt 5.2) applewebkit/531.2.2 (khtml, like gecko) chrome/25.0.899.0 safari/531.2.2")
         
+        #Adiciona os options setados para que o Browser rode com limitações ou nao
         option = webdriver.ChromeOptions()
+        
+        #Adiciona os user_agent 
         option.add_argument(f'user-agent={user_agent}')
+        
+        #Oculta o driver enquanto o Scrypt estiver funcionando
         #option.add_argument('--headless')
+        
+        #Faz a instalacao automatica do Browser selecionado evitando assim ter que ficar baixando manualmente
+        # e adiciona o service e options no Browser 
         servico = Service(ChromeDriverManager().install())
         driver = webdriver.Chrome(service=servico, options=option)
         
+        #Se existir link vai abrir o driver no link setado
         if self.link:
             driver.get(self.link)
         
+        #Retorna o driver
         return driver
